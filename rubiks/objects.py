d5778c3efa607fed79846133ebece374c1d7fc9d
loezasierra <joseloeza@gmail.com>
2022-07-09 22:28:04 -0700

    Move constants to objects folder

---
+++ rubiks/objects.py
@@ -0,0 +1,16 @@
+# Data objects for Rubik's Cube game
+
+import pygame
+
+
+## Constants ##
+
+class constants:
+    # Screen
+    TITLE = "Rubik's Cube"
+    WIDTH = 800
+    HEIGHT = 600
+    SCREEN_START = (0, 0)
+
+    # Colors
+    WHITE = (255, 255, 255)
--- rubiks/rubiks.py
+++ rubiks/rubiks.py
@@ -1,32 +1,19 @@
 # A simple Rubik's Cube game
 
 import pygame
-
-## Constants ##
-
-# Screen
-TITLE  = "Rubik's Cube"
-WIDTH  = 800
-HEIGHT = 600
-
-SCREEN_START = (0, 0)
-
-# Colors
-WHITE = (255, 255, 255)
+from objects import constants as cn
 
 
-## Game ##
-
-def main():
+def game():
     # Initialize screen
     pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption(TITLE)
+    screen = pygame.display.set_mode((cn.WIDTH, cn.HEIGHT))
+    pygame.display.set_caption(cn.TITLE)
 
     # Background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill(WHITE)
+    background.fill(cn.WHITE)
 
     # Event loop
     while True:
@@ -35,11 +22,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        screen.blit(background, SCREEN_START)
+        screen.blit(background, cn.SCREEN_START)
         pygame.display.update()
 
 
 
-# Run main
+# Run game
 if __name__ == '__main__':
-    main()
+    game()
